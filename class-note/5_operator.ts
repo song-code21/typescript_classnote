72f88978ca8352daa65e8ab4b45ff51d8e4ce8be
song-code21 <wkcjf131313@gmail.com>
2022-01-26 06:52:45 +0900

    인터섹션타입

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -31,3 +31,31 @@ function logMessage3(value: string | number) {
 }
 logMessage3('hello');
 logMessage3(100);
+
+//유니온타입 특징
+interface Developer {
+  name: string;
+  skill: string;
+}
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+function askSomeone(someone: Developer | Person) {
+  someone.name
+  // someone.age
+  // someone.skill
+}
+//인터페이스를 유니온타입으로 사용할 경우 보장된 공통 속성만 접근이 가능하다.
+
+
+//인터섹션타입 & 연산자
+function askSomeone2(someone: Developer & Person) {
+  someone.name
+  someone.age
+  someone.skill
+}
+// 인터섹션타입으로 정의하게 되면 모두 만족하는 타입이기 때문에 Developer와 Person의 모든 속성에 접근 가능하다.
+// 실무에서는 유니온타입을 더 많이 쓰게 된다. 
