573c1386e1769f0464f6c6c0bf9f6d18cf030330
song-code21 <wkcjf131313@gmail.com>
2022-01-30 17:18:37 +0900

    제네릭 타입(인터페이스)

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -1,3 +1,6 @@
+//제네릭이란 타입을 마치 함수의 파라미터처럼 사용하는 것을 말한다.
+// 데이터의 타입을 일반화한다.
+
 function logText(text) {
   console.log(text);
   return text;
@@ -13,3 +16,14 @@ function logText1<T>(text: T): T {
 }
 
 logText1<string>('하이');
+
+
+//제네릭 타입
+interface GenericTextLogFn {
+  <T>(text: T): T;
+}
+function logText2<T>(text: T): T {
+  return text;
+}
+
+let myString: GenericTextLogFn = logText2; // Okay;
